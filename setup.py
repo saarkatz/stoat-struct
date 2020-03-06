e0d3aa8de368156e871ef9a4b0db3474f7f994c2
Saar Katz <kats.saar@gmail.com>
2020-03-06 18:24:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
 
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 setup(
     name="stoat",
+    version="0.0.1",
+    # author="Saar Katz",
+    # author_email="kats.saar@gmail.com",
+    description="A package for declarative definition of binary structures",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saarkatz/stoat-struct",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
 )
