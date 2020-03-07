edc75a187155ef5a85610e5f9482652844988626
Saar Katz <kats.saar@gmail.com>
2020-03-07 12:56:31 +0200

    Fix bug in compound_byte
    Add test for CompoundByte

--- src/stoat/compound_byte.py
+++ src/stoat/compound_byte.py
@@ -24,26 +24,6 @@ class TinyField:
         self._value = value
 
 
-ATTRIBUTES = ('_accessor_target', '_accessor_key')
-
-
-class Accessor(type):
-    def __new__(mcs, target, *args):
-        dictionary = dict()
-        for attribute in dir(target):
-            if attribute.startswith('_'):
-                continue
-            value = getattr(target, attribute)
-            if callable(value):
-                dictionary[attribute] = partial(value, *args)
-            else:
-                dictionary[attribute] = property(
-                    lambda s: getattr(target, attribute),
-                    lambda s, v: setattr(target, attribute, v))
-        result = super().__new__(mcs, 'Accessor', tuple(), dictionary)
-        return result()
-
-
 class CompoundByte(Structure):
     def __init__(self, slices, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -57,8 +37,6 @@ class CompoundByte(Structure):
 
         self.slices = slices
         self.parts = []
-        if kwargs.get('shallow', False):
-            return
         for size in slices:
             self.parts.append(TinyField(size))
 
@@ -78,48 +56,51 @@ class CompoundByte(Structure):
     def unpack_from(cls, buffer, offset, *args, **kwargs):
         result, offset = super().unpack_from(buffer, offset, *args, **kwargs)
         assignment = result._set(buffer[offset])
-        if assignment:
+        if not assignment:
             assignment = result
         return assignment, offset + 1
 
     def _set(self, value, **kwargs):
+        # TODO: Handle errors better
         base_mask = 0b11111111
-        self.parts = []
+        error = None
         if isinstance(value, (bytes, bytearray)):
             value = value[0]
         try:
+            # Assume that the input is a single byte
             for i, size in enumerate(self.slices):
-                self.parts.append(TinyField(size))
-                self.parts[-1].value = (value & (
-                            base_mask >> sum(self.slices[:i]))) >> sum(
+                self.parts[i].value = (value & (
+                        base_mask >> sum(self.slices[:i]))) >> sum(
                     self.slices[i + 1:])
-        except ValueError as e:
+        except TypeError as e:
+            # Assume that the input is an array of integers
             try:
-                return super()._set(value, **kwargs)
-            except TypeError:
-                raise e
+                assert len(value) == len(self.parts)
+                for i, item in enumerate(value):
+                    self.parts[i].value = item
+            except AssertionError as a:
+                error = a
+            except ValueError as o:
+                # ValueError here means that it is an array but not of
+                # integers of inappropriate size
+                try:
+                    return super()._set(value, **kwargs)
+                except TypeError:
+                    error = o
+            except TypeError as u:
+                # TypeError here will occur if value isn't a list or a list
+                # of non integer types.
+                try:
+                    len(value)
+                except TypeError:
+                    error = e
+                else:
+                    error = u
+        if error:
+            raise error
 
     def __getitem__(self, item):
         return self.parts[item].value
 
     def __setitem__(self, key, value):
         self.parts[key].value = value
-
-
-if __name__ == '__main__':
-    class Test(Structure):
-        cb1 = CompoundByte + (1, 2, 3)
-        cb2 = CompoundByte + (1, 2, 3, 2)
-        cb3 = CompoundByte + [4]
-
-
-    t = Test()
-    t.cb1[0] = 1
-    t.cb1[1] = 3
-    t.cb1[2] = 7
-    t.cb1[3] = 3
-    t.cb2 = b'\xda'
-    t.cb3 = 129
-
-    print(t.pack())
-    print('-'.join((bin(v)[2:] for v in t.pack())))
