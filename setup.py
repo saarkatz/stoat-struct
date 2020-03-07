60e0606f552efd35424ac9ca859741d89c67b1ec
Saar Katz <kats.saar@gmail.com>
2020-03-07 12:37:26 +0200

    Move the "How Structure Works" section to docs/
    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="stoat",
     version="0.0.1",
-    # author="Saar Katz",
-    # author_email="kats.saar@gmail.com",
+    author="Saar Katz",
+    author_email="kats.saar@gmail.com",
     description="A package for declarative definition of binary structures",
-    # long_description=long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/saarkatz/stoat-struct",
     classifiers=[
